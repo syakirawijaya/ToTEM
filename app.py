14a5b9b27895588ff75ea5ccf4095d54a11e147d
syakirawijaya <90813516+syakirawijaya@users.noreply.github.com>
2024-03-15 22:23:02 +0000

    Incorporated TinyDB for database, succesfully uploaded umage and display it

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
-from flask import Flask, render_template, request, redirect, url_for, session
+from flask import Flask, render_template, request, redirect, url_for, session, send_from_directory
 from flask_qrcode import QRcode
 from werkzeug.utils import secure_filename
+from tinydb import TinyDB, Query
+from datetime import datetime
 import os
 
 app = Flask(__name__)
 qrcode = QRcode(app)
+db = TinyDB('db.json')
 
 # Configurations
 app.secret_key = 'testTOTEM2024'
@@ -25,20 +28,41 @@ def home():
     return render_template('index.html')
 
 
-
 @app.route('/upload', methods=['GET', 'POST'])
 def upload():
     if request.method == 'POST':
-        firstname = session.get('firstname')
-        lastname = session.get('lastname')
+        file = request.files['image']
+        description = request.form['description']
+        title = request.form['title']
+        if file:
+            filename = secure_filename(file.filename)
+            filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            file.save(filepath)
+            timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
+            user_route = f"{session.get('firstname').lower()}_{session.get('lastname').lower()}_{timestamp}"
+            db.insert({
+                'firstname': session.get('firstname'),
+                'lastname': session.get('lastname'),
+                'title': title,
+                'description': description,
+                'filename': filename,
+                'route': user_route
+            })
+            return redirect(url_for('user_page', user_route=user_route))
+    return render_template('uploadForm.html')
 
-        return redirect(url_for('qrcode'))
-        
-    return render_template('uploadForm.html', firstname=session.get('firstname'), lastname=session.get('lastname'))
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
-@app.route('/QR_code', methods=['GET', 'POST'])
-def qrcode():
-    return render_template('QRcodeGenerator.html')
+@app.route('/<user_route>')
+def user_page(user_route):
+    User = Query()
+    user_data = db.search(User.route == user_route)
+    if user_data:
+        user_data = user_data[0]
+        return render_template('user_page.html', user_data=user_data)
+    return 'User not found', 404
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
